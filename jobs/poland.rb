d4ce0a932097743a687fab07e015c36c7cefa17c
Arkadiusz Buras <me@macbury.ninja>
2020-04-25 15:15:26 +0200

    Wilgotnosc gleby

--- jobs/poland.rb
+++ jobs/poland.rb
@@ -5,6 +5,10 @@ red = NodeRed.new
 SCHEDULER.every '10m', first_in: 0 do |job|
   data = red.poland
 
+  send_event('wilgotnosc-gleby', { 
+    value: data.dig('wilgotnoscGleby', 1, 'last').round
+  })
+
   data.each do |key, stats|
     start = stats[1]['last']&.round(2)
     last = stats[0]['last']&.round(2)
